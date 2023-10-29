69809686a1bec54e07b32dd563305eaf97a0ba79
yesiah <hyenhsiang@gmail.com>
2023-10-29 18:33:07 +0800

    Checkpoint 1.4: update keyboard with another keyboard (not editing)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var deployment_id = "AKfycbxYSYHkeHTixcbmYDF21g0HOwfiqgpTIw-UaMi9V5V5akxnEVD6jJ3a-yw9zkAZsokS"
+var deployment_id = "AKfycby8vrNezOwESZUSzN62MCg3QQ6Zwi_dLboDs_FYaWN3Ta9Nx9MyOlS9gv-IKuP9toe4"
 var server_url = "https://script.google.com/macros/s/" + deployment_id + "/exec"
-var version_text = "Test 1.5.20: test multilevel keyboard with optional chaining"
+var version_text = "Test 1.5.24: test update keyboard with another keyboard"
 
 // Steps to update webhook:
 //   1. Deploy project
@@ -29,14 +29,12 @@ function get_sheet() {
 }
 
 function write_to_last_row(data) {
-  var ss = SpreadsheetApp.openById("1rjePekaWarx5lojXcl8pYwWxKblrEYvhXj7Y6If_zSs");  // In nn Out Data
-  var sheet = ss.getSheetByName("Data");
-  sheet.appendRow([data]);
+  let sheet = get_sheet();
+  sheet.appendRow(data);
 }
 
 function test(e) {
-  var ss = SpreadsheetApp.openById("1rjePekaWarx5lojXcl8pYwWxKblrEYvhXj7Y6If_zSs");  // In nn Out Data
-  var sheet = ss.getSheetByName("Data");
+  let sheet = get_sheet();
   sheet.clear();
   var range = sheet.getRange("A1");
   range.setValue(e);
@@ -47,48 +45,8 @@ function test(e) {
 
 // e is the telegram Update object.
 function identifier(update) {
-  write_to_last_row("iden");
-  let sheet = get_sheet();
-  sheet.appendRow(["identifier update props"]);
-  Object.keys(update).forEach((prop) => sheet.appendRow([prop]));
-  sheet.appendRow(["update.callback_query", update.callback_query]);
-
-  // if (update.callback_query) {
-  //   sheet.appendRow(["DETECTED!"]);
-  // } else if (update.message.text) {
-  //   sheet.appendRow(["test detected."]);
-  // }
-
-  // if (update.message) {
-  //   sheet.appendRow(["message detected."]);
-  // } else if (update.callback_query) {
-  //   sheet.appendRow(["DETECTED!"]);
-  // }
-
-  sheet.appendRow(["check message"]);
-  if (update?.message) {
-    sheet.appendRow(["message detected."]);
-  }
-  Object.keys(update).forEach((prop) => sheet.appendRow([prop]));
-
-  sheet.appendRow(["check callback_query"]);
-  if (update?.callback_query) {
-    sheet.appendRow(["callback detected."]);
-  }
-  Object.keys(update).forEach((prop) => sheet.appendRow([prop]));
-
-  sheet.appendRow(["check message.text"]);
   if (update?.message?.text) {
-    sheet.appendRow(["message.text detected."]);
-  }
-  Object.keys(update).forEach((prop) => sheet.appendRow([prop]));
-
-  sheet.appendRow(["NO crash after tests"]);
-
-  if (update.message) {
-    write_to_last_row("is text");
     if (update.message.text == '/test') {
-      write_to_last_row("called /test");
       // Try make a keyboard
       // var reply_keyboard = {
       //   keyboard: [["DBS", "PChome"], ["iPass"]],
@@ -145,64 +103,56 @@ function identifier(update) {
         "text": update.message.text,
       }
     }
-  }
-
-  if (update.callback_query) {
-    write_to_last_row("callback");
+  } else if (update?.callback_query) {
+    write_to_last_row(["callback"]);
     if (update.callback_query.data == "clicked google") {
-      write_to_last_row("clicked google");
+      write_to_last_row(["clicked google"]);
       var inline_keyboard2 = {
         inline_keyboard: [
           [
             {
               text: "google1",
               callback_data: "clicked google1",
-            }
-          ],
-          [
+            },
             {
               text: "google2",
               callback_data: "clicked google2",
-            },
-            {
-              text: "google3",
-              callback_data: "clicked google3",
             }
           ]
         ]
       };
 
+      // var mensaje = {
+      //   "method": "editMessageText",
+      //   "chat_id": String(update.callback_query.from.id),
+      //   "message_id": String(update.callback_query.message.message_id),
+      //   "text": version_text,
+      //   "reply_markup": JSON.stringify(inline_keyboard2),
+      // }
       var mensaje = {
-        "method": "editMessageText",
-        "chat_id": String(update.callback_query.from.id),
-        "message_id": String(update.callback_query.message.message_id),
+        "method": "sendMessage",
+        "chat_id": String(update.callback_query.message.chat.id),
         "text": version_text,
         "reply_markup": JSON.stringify(inline_keyboard2),
       }
     }
   }
 
-  write_to_last_row(mensaje);
-  console.log(String(update.message.chat.id));
+  write_to_last_row(["sent", mensaje]);
   return mensaje
 }
 
 // This script is published as a web app. 
 // doPost(e) will be triggered upon receiving a POST request.
 function doPost(e) {
-  var sheet = get_sheet();
-  sheet.appendRow(["doPost", e.postData.contents]);
+  write_to_last_row(["received", e.postData.contents]);
 
   var tg_update = JSON.parse(e.postData.contents);
-  sheet.appendRow(["tg_update props"]);
-  Object.keys(tg_update).forEach((prop) => sheet.appendRow([prop]));
-  sheet.appendRow(["tg_update.callback_query", tg_update.callback_query]);
   var payload = identifier(tg_update);
   var data = {
     "method": "post",
     "payload": payload
   }
+
   UrlFetchApp.fetch("https://api.telegram.org/bot" + token + "/", data);
 }
-
-
